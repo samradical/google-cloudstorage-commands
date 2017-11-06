e69c931c4363be266489b26a1766760c9fce08d2
samelie <sam@stinkdigital.com>
2017-11-06 13:28:41 -0800

    fix

--- example.js
+++ example.js
@@ -1,10 +1,22 @@
 const GOOGLE = require('./index')
 var fs = require('fs');
 var path = require('path');
-const BUCKET_NAME = 'samrad-deuxtube/'
+const BUCKET_NAME = 'orchard-lane/'
 const BUCKET = `gs://${BUCKET_NAME}`
-
+/*
 return GOOGLE.upload("some/directory", "gs://soe_bucket/path...", force: boolean)
+  .then(() => {})
+  .catch(err => {
+    console.log(err);
+  })
+*/
+  return GOOGLE.cors(BUCKET)
+  .then(() => {})
+  .catch(err => {
+    console.log(err);
+  })
+
+return GOOGLE.publicRead(BUCKET)
   .then(() => {})
   .catch(err => {
     console.log(err);
